ef559988ceb23d3b138cf27317f1a287f68042cd
Jeremy Krinsley <krinsley@gmail.com>
2020-11-11 16:56:38 -0500

    clamping fp

--- src/Releases/Release0014_Aummaah/Scene.js
+++ src/Releases/Release0014_Aummaah/Scene.js
@@ -5,23 +5,30 @@ import Glitchy from './glitchy/index.js'
 import Orbit from '../../Common/Controls/Orbit';
 import Flying from '../../Common/Controls/Flying'
 import { MaterialsProvider } from './MaterialsContext.js';
+import FirstPerson from '../../Common/Controls/FirstPerson'
+import * as THREE from 'three';
 
 // crazy glitch 778e1868d6e437efd56591ac910226644d883c0f
 // flat tiling a76fc468aae5e1d690c87a9935a71a797c1ae8a2
 
 export function Scene({ }) {
-    const {camera} = useThree()
+    const {camera, raycaster } = useThree()
     useEffect(() => {
         console.log(camera.position)
         camera.position.set(0,.1,5)
         // camera.lookAt(0, 0, 0)
     }, [])
     useFrame(() => {
-        camera.position.z -= .01
+        // fly forward thru the scene
+        // camera.position.z -= .01
+        camera.position.set(camera.position.x, THREE.Math.clamp(camera.position.y, .1, .5), camera.position.z)
+        // console.log("raycaster", raycaster)
+        // camera.lookAt(raycaster.ray.direction.x, raycaster.ray.direction.y, 0)
+        // console.log(raycaster.ray.direction.x, raycaster.ray.direction.y, 0)
         // console.log('cam pos', camera.position)
     })
     return <>
-        {/* <Orbit autoRotate={false} /> */}
+        <FirstPerson autoRotate={false} heightMax={.1} heightMin={.1} heightSpeed={true} heightCoefficient={-1}/>
         <ambientLight />
         <MaterialsProvider>
             <Sports />
