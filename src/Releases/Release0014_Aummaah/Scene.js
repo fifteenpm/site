3b838bdbd66b21c19815c672dd49b02c48af4ebc
Jeremy Krinsley <krinsley@gmail.com>
2020-11-11 16:41:45 -0500

    flying thru tennis balls

--- src/Releases/Release0014_Aummaah/Scene.js
+++ src/Releases/Release0014_Aummaah/Scene.js
@@ -1,16 +1,27 @@
 import React, { useEffect } from 'react';
+import {useFrame, useThree} from 'react-three-fiber';
 import Sports from './Sports/Sports.js'
 import Glitchy from './glitchy/index.js'
-
+import Orbit from '../../Common/Controls/Orbit';
 import Flying from '../../Common/Controls/Flying'
 import { MaterialsProvider } from './MaterialsContext.js';
-import { Suspense } from 'react';
+
 // crazy glitch 778e1868d6e437efd56591ac910226644d883c0f
 // flat tiling a76fc468aae5e1d690c87a9935a71a797c1ae8a2
 
 export function Scene({ }) {
+    const {camera} = useThree()
+    useEffect(() => {
+        console.log(camera.position)
+        camera.position.set(0,.1,5)
+        // camera.lookAt(0, 0, 0)
+    }, [])
+    useFrame(() => {
+        camera.position.z -= .01
+        // console.log('cam pos', camera.position)
+    })
     return <>
-        <Flying />
+        {/* <Orbit autoRotate={false} /> */}
         <ambientLight />
         <MaterialsProvider>
             <Sports />
--- src/Releases/Release0014_Aummaah/Sports/Sports.js
+++ src/Releases/Release0014_Aummaah/Sports/Sports.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useContext, useRef, useState, useMemo } from 'react';
 import { Canvas, extend, useFrame, useThree } from 'react-three-fiber';
 import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import TileGenerator from "../../../Common/Utils/TileGenerator";
 import { Tennis } from "./Tennis";
 import { MaterialsContext } from '../MaterialsContext';
@@ -13,16 +12,6 @@ export default function Sports() {
 
     const materials = useContext(MaterialsContext);
     const [tileGridSize, setTileGrideSize] = useState(10);
-
-
-    // TODO This is forcing a 2d view but is probably very computationally expensive - orthographic camera? 
-    useFrame(() => {
-        // let lookAtPos = camera.position.copy(); // TODO this is erroring on 'Cannot read property 'x' of undefined'
-        let lookAtPos = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
-        lookAtPos.y = 0;
-        camera.position.y = 3.;
-        camera.lookAt(lookAtPos);
-    })
 
 
     return (<TileGenerator
