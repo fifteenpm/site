ca2a79ab5ce10441a97eb472826bb13ea3a95cd5
Jeremy Krinsley <krinsley@gmail.com>
2020-11-11 19:26:51 -0500

    buggy clamping logic

--- src/Releases/Release0014_Aummaah/Scene.js
+++ src/Releases/Release0014_Aummaah/Scene.js
@@ -1,42 +1,43 @@
-import React, { useEffect } from 'react';
-import {useFrame, useThree} from 'react-three-fiber';
-import Sports from './Sports/Sports.js'
-import Glitchy from './glitchy/index.js'
-import Orbit from '../../Common/Controls/Orbit';
-import Flying from '../../Common/Controls/Flying'
-import { MaterialsProvider } from './MaterialsContext.js';
-import FirstPerson from '../../Common/Controls/FirstPerson'
+import React, { Suspense, useEffect } from 'react';
+import { useFrame, useThree } from 'react-three-fiber';
 import * as THREE from 'three';
+import FirstPerson from '../../Common/Controls/FirstPerson';
+import { MaterialsProvider } from './MaterialsContext.js';
+import Sports from './Sports/Sports.js';
 // crazy glitch 778e1868d6e437efd56591ac910226644d883c0f
 // flat tiling a76fc468aae5e1d690c87a9935a71a797c1ae8a2
 
 export function Scene({ }) {
-    const {camera, raycaster } = useThree()
+    const { camera, raycaster } = useThree()
     useEffect(() => {
-        console.log(camera.position)
-        camera.position.set(0,.1,5)
-        camera.lookAt(0, 0, 0)
-        
+        // console.log(camera.position)
+        // camera.position.set(0, 30, 5)
+        camera.lookAt(0, 1, 0)
+        camera.rotation.x = -2
+        camera.position.y = .9
     }, [])
     useFrame(() => {
-        console.log(camera.rotation)
-//         _x: -1.2632205015650546
-// _y: -0.02564309799712867
-// _z: -0.08054237130475483
+        // console.log(camera.rotation)
+        // this was the rotation that looked good:
+        // _x: -1.2632205015650546
+        // _y: -0.02564309799712867
+        // _z: -0.08054237130475483
         // fly forward thru the scene
         // camera.position.z -= .01
-        camera.position.set(camera.position.x, THREE.Math.clamp(camera.position.y, .4, .5), camera.position.z)
-        camera.rotation.set(THREE.Math.clamp(camera.rotation.x, -1.26, -1.2), camera.rotation.y, camera.rotation.z)
-        // console.log("raycaster", raycaster)
-        // camera.lookAt(raycaster.ray.direction.x, raycaster.ray.direction.y, 0)
-        // console.log(raycaster.ray.direction.x, raycaster.ray.direction.y, 0)
-        // console.log('cam pos', camera.position)
+        camera.position.set(camera.position.x, THREE.Math.clamp(camera.position.y, .75, .8), camera.position.z)
+        camera.rotation.set(
+            THREE.Math.clamp(camera.rotation.x, -1.3, -1.1),
+            THREE.Math.clamp(camera.rotation.y, -.05, .05),
+            THREE.Math.clamp(camera.rotation.z, -.05, .05),
+        )
     })
     return <>
-        <FirstPerson autoRotate={false} heightMax={.1} heightMin={.1} heightSpeed={true} heightCoefficient={-1}/>
+        <FirstPerson autoRotate={false} heightMax={.1} heightMin={.1} heightSpeed={true} heightCoefficient={-1} />
         <ambientLight />
         <MaterialsProvider>
-            <Sports />
+            <Suspense fallback={null}>
+                <Sports />
+            </Suspense>
         </MaterialsProvider>
     </>
 }
